1e9f90498d9f5ee9efaba53521ca60df22652061
miguelorg12 <miguelvillalpando19@gmail.com>
2024-11-27 23:50:46 -0600

    change enviroment and compiled

--- src/app/environments/enviroment.ts
+++ src/app/environments/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     //apiurl: 'http://192.168.100.10:3000/api',
     //socketUrl: 'http://192.168.100.10:3002',
-    apiurl: 'http://localhost:3000/api',
-    socketUrl: 'http://localhost:3002',
+    apiurl: 'http://54.91.178.233:3000/api',
+    socketUrl: 'http://54.91.178.233:3002',
     production: false
 };
